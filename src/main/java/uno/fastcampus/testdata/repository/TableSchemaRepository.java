9d19a4d9989995190e84dd4943f8d928823ebfff
ChanceMissed <bigchancemiss@gmail.com>
2024-08-28 16:13:26 +0900

    #28 - JPA 비즈니스 로직용 사용자 쿼리 메소드와 테스트 추가
    실제로 서비스에서 이용할 것으로 예상하는 쿼리 3종을 추가하고 각각 정상 시나리오 테스트.
    
    * 특정 사용자가 가지고 있는 테이블 스키마 전체 목록 조회
    * 특정 사용자와 특정 이름의 테이블 스키마 1개 조회
    * 특정 사용자와 특정 이름의 테이블 스키마 삭제

--- src/main/java/uno/fastcampus/testdata/repository/TableSchemaRepository.java
+++ src/main/java/uno/fastcampus/testdata/repository/TableSchemaRepository.java
@@ -1,8 +1,19 @@
 package uno.fastcampus.testdata.repository;
 
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uno.fastcampus.testdata.domain.TableSchema;
 
 public interface TableSchemaRepository extends JpaRepository<TableSchema, Long> {
-}
 
+    // 목록조회
+    Page<TableSchema> findByUserId(String userId, Pageable pageable);
+
+    // 특정 스키마 조회
+    Optional<TableSchema> findByUserIdAndSchemaName(String userId, String schemaName);
+
+    // 스키마 삭제
+    void deleteByUserIdAndSchemaName(String userId, String schemaName);
+}
